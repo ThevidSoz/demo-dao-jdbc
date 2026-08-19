a0d856d1a2c4a0a2a76f91cf53efd562b86356a8
ThevidSoz <souzapx.david@gmail.com>
2026-08-19 17:17:44 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,10 @@ public class Program {
 		Departament obj = new Departament(1, "Books");
 		
 		System.out.println(obj);
+		
+		Seller sellerObj = new Seller(21, "Douglas", "d.oublas@email.com", new Date(), 3000.0, obj);
+		
+		System.out.println(sellerObj);
 
 	}
 
